3c4032bd237369d282f8fadfa5ed1173ef91b501
Bibek <bibek9818917036@gmail.com>
2023-08-09 00:01:02 +1000

    added close button

--- src/App.js
+++ src/App.js
@@ -10,42 +10,51 @@ const messages = [
 
 const App = () => {
   const [step, setStep] = useState(1);
-  
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlePrevious = () => {
-    step > 1 && setStep(step - 1);
+    step > 1 && setStep((s) => s - 1);
   };
   const handleNext = () => {
-    step < 3 && setStep(step + 1);
+    step < 3 && setStep((s) => s + 1);
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-
-      <p className="message">
-        Step {step}:{messages[step - 1]}
-      </p>
-
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}:{messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+      <button
+        onClick={() => setIsOpen((is) => !is)}
+        style={{ backgroundColor: "#7950f2", color: "white" }}
+        className="close"
+      >
+        &times;
+      </button>
+    </>
   );
 };
 
